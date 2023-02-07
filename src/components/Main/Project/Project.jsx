d84dc9b4686025d0c7ae8f7a945afaac6c24014e
nurbek <nurbek.itu@gmail.com>
2023-02-07 18:03:34 +0600

    Finished with the projects section and now we need to fix it a little

--- src/components/Main/Project/Project.jsx
+++ src/components/Main/Project/Project.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Title from '../../UI/Title/Title';
 import cls from './Project.module.scss';
-import first from '../../../assets/img/first.png';
-import second from '../../../assets/img/second.png';
-import third from '../../../assets/img/third.png';
 import { cardList } from '../../../utils/cardList';
+import second from '../../../assets/img/second.png';
+import first from '../../../assets/img/first.png';
 
 const Project = () => {
   return (
@@ -16,10 +15,10 @@ const Project = () => {
             <li className={cls.grey}></li>
             <li className={cls.orange}></li>
             <li className={cls.img1}>
-              <img src={first} alt='justPhoto' />
+              <img className={cls.justPhoto1} src={first} alt='justPhoto' />
             </li>
             <li className={cls.img2}>
-              <img src={second} alt='justPhoto' />
+              <img className={cls.justPhoto2} src={second} alt='justPhoto' />
             </li>
             {cardList?.map((el) => (
               <li
@@ -29,7 +28,9 @@ const Project = () => {
                   backgroundColor: el.id % 2 === 0 ? '#3B91D1' : '#F6C222',
                 }}
               >
-                <a href={el.route}>{el.alt}</a>
+                <a href={el.route} target='_blank' rel='noreferrer'>
+                  <span>{el.alt}</span>
+                </a>
               </li>
             ))}
           </ul>
